22b5f53bc55c26d2cc46767eaffdbac802f80f80
Gavin Kerr <gkerr708@gmail.com>
2023-11-20 23:17:50 -0400

    Laptop

--- lua/key_mappings.lua
+++ lua/key_mappings.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.keymap.set('n', '<Leader>fe', vim.cmd.Ex) -- opens file explorer
-vim.keymap.set('n', '<Leader>rm', ':! clear; python3 %<CR>') -- % means current file
+vim.keymap.set('n', '<Leader>rm', ':! python3 %<CR>') -- % means current file
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
