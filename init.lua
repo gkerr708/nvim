c4fafb8cadb33b099fdba57d6fec3979e50e42bb
gkerr_laptop <gavin.kerr@dal.ca>
2025-08-13 13:35:56 -0300

    fixed warning message

--- init.lua
+++ init.lua
@@ -4,6 +4,13 @@ vim = vim or {}
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
+
+-- LSP compat shim for plugins still calling vim.lsp.buf_get_clients()
+--@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.buf_get_clients = function(bufnr)
+  return vim.lsp.get_clients({ bufnr = bufnr })
+end
+
 require("config.lazy")
 require("settings")
 require("key_mappings")
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,10 +1,52 @@
 return {
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-nvim-lsp',
-    },
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'L3MON4D3/LuaSnip',                 -- Snippet engine
+    'saadparwaiz1/cmp_luasnip',        -- Snippet completions
+    'hrsh7th/cmp-nvim-lsp',            -- LSP source for cmp
+    'hrsh7th/cmp-buffer',              -- Buffer words source
+    'hrsh7th/cmp-path',                -- File paths
   },
+  opts = function()
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+
+    return {
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert {
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+
+        ['<CR>'] = cmp.mapping.confirm { select = true },
+        ['<C-Space>'] = cmp.mapping.complete(),
+      },
+      sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+      },
+    }
+  end,
 }
