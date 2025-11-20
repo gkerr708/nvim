d4829d94fb95fc66b776e9525981f0b38cb38174
gkerr_laptop <gavin.kerr@dal.ca>
2025-11-20 11:35:06 -0400

    Black background

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,40 +1,95 @@
-
 return{
   'nvim-tree/nvim-web-devicons',
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require("catppuccin").setup({
-        flavour = "macchiato",   -- latte | frappe | macchiato | mocha
-        transparent_background = false,
-        integrations = {
-          lualine = true,
-          nvimtree = false,
-          telescope = true,
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'iceberg_dark',
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+            refresh_time = 16, -- ~60fps
+            events = {
+              'WinEnter',
+              'BufEnter',
+              'BufWritePost',
+              'SessionLoadPost',
+              'FileChangedShellPost',
+              'VimResized',
+              'Filetype',
+              'CursorMoved',
+              'CursorMovedI',
+              'ModeChanged',
+            },
+          }
         },
-      })
-      vim.cmd.colorscheme("catppuccin")
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+      }
     end,
   },
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = "catppuccin",
-        component_separators = "|",
-        section_separators = "",
-      },
-    },
-  },
+  --{
+  --  "catppuccin/nvim",
+  --  name = "catppuccin",
+  --  priority = 1000,
+  --  config = function()
+  --    require("catppuccin").setup({
+  --      flavour = "macchiato",   -- latte | frappe | macchiato | mocha
+  --      transparent_background = true,
+  --      integrations = {
+  --        lualine = true,
+  --        nvimtree = false,
+  --        telescope = true,
+  --      },
+  --    })
+  --    vim.cmd.colorscheme("catppuccin")
+  --  end,
+  --},
   --{
   --  'folke/tokyonight.nvim',
   --  priority = 1000,
   --  config = function()
   --    require('tokyonight').setup({
   --      style = 'night',       -- 'storm' | 'night' | 'moon' | 'day'
+
+  --      integrations = {
+  --        lualine = true,
+  --        nvimtree = false,
+  --        telescope = true,
+  --      },
+
   --      transparent = true,
   --      styles = {
   --        sidebars = 'transparent',
@@ -44,36 +99,14 @@ return{
   --    vim.cmd.colorscheme('tokyonight')
   --  end,
   --},
-  --{
-  --  'nvim-lualine/lualine.nvim',
-  --  opts = {
-  --    options = {
-  --      icons_enabled = false,
-  --      theme = 'tokyonight',
-  --      component_separators = '|',
-  --      section_separators = '',
-  --    },
-  --  },
-  --},
-  --{
-  --  'navarasu/onedark.nvim',
-  --  priority = 1000,
-  --  config = function()
-  --    require('onedark').setup {
-  --      transparent = true,
-  --    }
-  --    require('onedark').load()
-  --  end,
-  --},
-  --{
-  --  'nvim-lualine/lualine.nvim',
-  --  opts = {
-  --    options = {
-  --      icons_enabled = false,
-  --      theme = 'onedark',
-  --      component_separators = '|',
-  --      section_separators = '',
-  --    },
-  --  },
-  --},
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      require('onedark').setup {
+        transparent = true,
+      }
+      require('onedark').load()
+    end,
+  },
 }
