fe2c2818009ec09d981504041265e358bffbb9f0
gkerr_laptop <gavin.kerr@dal.ca>
2026-03-27 22:14:59 -0300

    Finally fixed the treesitter error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,22 @@
 -- lua/plugins/treesitter.lua
 return {
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter', -- Sets main module to use for opts
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html',
-        'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 
-        'vim', 'vimdoc', 'yaml', 'json', 'javascript', 'typescript', 
-        'css', 'scss', 'tsx', 'python', 'go', 'rust', 'r', 'rnoweb', 'latex', 'yaml' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-  },
+    config = function()
+      require('nvim-treesitter').setup()
 
+      -- Enable treesitter highlighting for all filetypes with a parser
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(ev)
+          local ok = pcall(vim.treesitter.start)
+          if not ok then
+            -- No parser for this filetype, fall back to regex highlighting
+            vim.bo[ev.buf].syntax = 'on'
+          end
+        end,
+      })
+    end,
+  },
 }
