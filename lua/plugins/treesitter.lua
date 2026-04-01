0d373138d331bb26e64b131f1635ab0e80c50515
gkerr_laptop <gavin.kerr@dal.ca>
2026-04-01 14:21:04 -0300

    Fixed another damn merge conflict

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,20 +2,14 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-<<<<<<< HEAD
     branch = 'master',
-||||||| 0edc77e
-=======
-    lazy = false,
->>>>>>> refs/remotes/origin/main
     build = ':TSUpdate',
-<<<<<<< HEAD
     main = 'nvim-treesitter.configs',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html',
         'lua', 'luadoc', 'markdown', 'markdown_inline', 'query',
         'vim', 'vimdoc', 'yaml', 'json', 'javascript', 'typescript',
-        'css', 'scss', 'tsx', 'python', 'go', 'rust', 'r', 'rnoweb', 'yaml'},
+        'css', 'scss', 'tsx', 'python', 'go', 'rust', 'r', 'rnoweb', 'yaml', 'latex'},
       auto_install = true,
       highlight = {
         enable = true,
@@ -23,37 +17,5 @@ return {
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
-  },
-||||||| 0edc77e
-    main = 'nvim-treesitter', -- Sets main module to use for opts
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html',
-        'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 
-        'vim', 'vimdoc', 'yaml', 'json', 'javascript', 'typescript', 
-        'css', 'scss', 'tsx', 'python', 'go', 'rust', 'r', 'rnoweb', 'latex', 'yaml' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-  },
-=======
-    config = function()
-      require('nvim-treesitter').setup()
->>>>>>> refs/remotes/origin/main
-
-      -- Enable treesitter highlighting for all filetypes with a parser
-      vim.api.nvim_create_autocmd('FileType', {
-        callback = function(ev)
-          local ok = pcall(vim.treesitter.start)
-          if not ok then
-            -- No parser for this filetype, fall back to regex highlighting
-            vim.bo[ev.buf].syntax = 'on'
-          end
-        end,
-      })
-    end,
   },
 }
